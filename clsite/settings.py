a7e80e60403a2769f34983fb1b4784880f29856c
pavel <pogasanov@gmail.com>
2019-06-03 20:21:12 +0700

    use heroku pg

--- clsite/settings.py
+++ clsite/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
