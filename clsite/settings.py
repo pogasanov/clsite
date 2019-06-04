af016f340270b524a913beb0a8abe06421cc0aaf
pavel <pogasanov@gmail.com>
2019-06-04 14:12:59 +0700

    simplier path to global static

--- clsite/settings.py
+++ clsite/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "clsite/static"),
+    os.path.join(PROJECT_ROOT, "static"),
 ]
 
 # Simplified static file serving.
