ed1bb75f0e7a3caef711e17b41c572182c72c982
pavel <pogasanov@gmail.com>
2019-06-05 14:16:33 +0700

    save profile when User is added

--- clsite/settings.py
+++ clsite/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'profiles'
+    'profiles.apps.ProfilesConfig'
 ]
 
 SITE_ID = 1
