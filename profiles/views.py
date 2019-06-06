bf33fff9e600196036b0026be42bcccdbd099bfb
Muhammad Haziq <mhaziq@sparkcognition.com>
2019-06-06 22:53:01 +0500

    remove previous photo after adding new one

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
-
+import os
 from .forms import RegistrationForm
 
 
@@ -17,8 +17,14 @@ def profile(request, username=None):
     else:
         user = request.user
         if request.method == 'POST' and request.FILES['photo-input']:
+            previous_photo = user.profile.photo
             user.profile.photo = request.FILES['photo-input']
             user.profile.save()
+
+            # remove previous photo
+            if previous_photo.url.split('/')[2] != "dummy-img.png":
+                os.remove(previous_photo.url[1:])
+
             return JsonResponse({'url': user.profile.photo.url})
     return render(request, "profile-page.html", context={
         'selected_user': user
