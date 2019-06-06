ecd41eee4b74a11cfa986321c2a142088e9b6e25
pavel <pogasanov@gmail.com>
2019-06-06 17:29:35 +0700

    adds new fields to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -9,3 +9,9 @@ class Profile(models.Model):
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     jurisdiction = models.CharField(max_length=2, choices=USA_STATES, verbose_name='Jurisdiction', blank=True)
+    headline = models.CharField(max_length=120, verbose_name='Headline', blank=True)
+    bio = models.TextField(verbose_name='Bio', blank=True)
+    website = models.URLField(verbose_name='Website URL', blank=True)
+    twitter = models.CharField(max_length=50, blank=True)
+    linkedin = models.CharField(max_length=50, blank=True)
+    facebook = models.CharField(max_length=50, blank=True)
