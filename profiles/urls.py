9535e395817c68bd9cdfde2d2ee012e008d7de77
pavel <pogasanov@gmail.com>
2019-06-06 22:58:12 +0700

    auth user after registration and redirect to profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,5 @@
-from django.urls import path, reverse_lazy
+from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.views.generic.edit import CreateView
-from django.contrib.auth.forms import UserCreationForm
 
 from . import views
 
@@ -10,11 +8,7 @@ urlpatterns = [
 
     path('login', auth_views.LoginView.as_view(success_url='/'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
-    path('register', CreateView.as_view(
-        template_name='registration/register.html',
-        form_class=UserCreationForm,
-        success_url=reverse_lazy('login')
-    ), name='register'),
+    path('register', views.UserRegistrationView.as_view(), name='register'),
 
     path('profile', views.profile, name='profile'),
     path('profile/<username>', views.profile, name='profile'),
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
-from django.conf import settings
-
-from .forms import RegistrationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 
 def index(request):
@@ -19,3 +19,14 @@ def profile(request, username=None):
     return render(request, "profile-page.html", context={
         'selected_user': user
     })
+
+
+class UserRegistrationView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('profile')
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        login(request, self.object)
+        return response
