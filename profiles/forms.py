288a31adcd3a78447b647a12f8661f6803dd1445
pavel <pogasanov@gmail.com>
2019-06-06 22:58:23 +0700

    remove unused registration form

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,8 +1,1 @@
 from django import forms
-from django.contrib.auth.models import User
-
-
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
