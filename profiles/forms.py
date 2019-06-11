dbe1e2922eaafd7a7d4d9e852981da965cb3e7da
Muhammad Haziq <mhaziq@sparkcognition.com>
2019-06-11 15:31:25 +0500

    added first name and last name fields

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
 from .models import Profile
+from django import forms
 
 
 class ProfileForm(ModelForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=150)
+
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = ['first_name', 'last_name', 'jurisdiction', 'headline', 'bio', 'website', 'twitter', 'linkedin', 'facebook']
