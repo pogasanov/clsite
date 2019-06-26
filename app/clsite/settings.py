7ecb82e2b7b980cef52223c3b64da4ae29a2045d
pavel <pogasanov@gmail.com>
2019-06-26 21:43:07 +0700

    fix template directory bug

--- app/clsite/settings.py
+++ app/clsite/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'clsite/templates'
+            os.path.join(BASE_DIR, 'clsite/templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
