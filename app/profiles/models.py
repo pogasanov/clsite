694f7f2c6bb6ebc97f1decf65d31322892483453
Muhammad Haziq <mhaziq@sparkcognition.com>
2019-06-29 18:08:14 +0500

    updated migration removed choices

--- app/profiles/models.py
+++ app/profiles/models.py
@@ -7,7 +7,6 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 import os
 
 from .choices import USA_STATES
-from profiles.tags.utilities import get_all_tags_tuple
 
 
 class Address(models.Model):
@@ -161,7 +160,7 @@ class Profile(AbstractUser):
         verbose_name='Jurisdiction', blank=True, null=True
     )
     law_type_tags = ArrayField(
-        models.IntegerField(choices=get_all_tags_tuple()),
+        models.IntegerField(),
         verbose_name='Law Type Tags', blank=True, null=True
     )
     headline = models.CharField(max_length=120, verbose_name='Headline', blank=True)
