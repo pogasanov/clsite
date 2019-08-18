9582373cbe104da30f9724d8f460ec570ab0ae7d
Saad Waqar <saadwaqarmughal@gmail.com>
2019-08-18 15:17:17 +0500

    Added Transaction proof to add transaction flow

--- src/js/transaction.js
+++ src/js/transaction.js
@@ -6,10 +6,12 @@ $(document).ready(function () {
     const recommendationFieldSelector = '#id_requester_recommendation, #id_requestee_recommendation';
     const recommendationLabelSelector = '[for="id_requester_recommendation"], [for="id_requestee_recommendation"]';
     const confirmTransactionSelector = '.confirm-transaction-fields';
+    const transactionProofImageSelector = '#id_proof_receipt_requester_image';
 
     $(recommendationFieldSelector).hide();
     $(recommendationLabelSelector).hide();
     $(confirmTransactionSelector).hide();
+    $(transactionProofImageSelector).hide();
 
     $('#add-recommendation').on('click', function (e) {
         e.preventDefault();
@@ -24,4 +26,17 @@ $(document).ready(function () {
         $('.deny-transaction').hide();
         $(confirmTransactionSelector).show();
     });
-})
+    
+    $("#id_proof_receipt_requester").change(function(){
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $(transactionProofImageSelector).attr('src', e.target.result);
+            };
+
+            $(transactionProofImageSelector).show();
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+});
