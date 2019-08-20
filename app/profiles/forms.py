32b3005e7ca042bd69170bdc1602e0b8c1090cc8
Saad Waqar <saadwaqarmughal@gmail.com>
2019-08-21 02:57:54 +0500

    changed label for transaction proof

--- app/profiles/forms.py
+++ app/profiles/forms.py
@@ -268,7 +268,7 @@ class TransactionForm(ModelForm):
         self.fields['currency'].widget.attrs['class'] = 'form-control col-md-4 mr-2'
 
         self.fields['is_requester_principal'].label = 'Did one of you pay the other?'
-        self.fields['proof_receipt_requester'].label = 'Transaction Proof(Optional)'
+        self.fields['proof_receipt_requester'].label = 'Screenshot of wire transfer (Optional)'
         self.fields['requester_recommendation'].label = 'Write a brief written recommendation'
         self.fields['requester_review'].label = 'Would you work with them again?'
         self.fields['date'].label = 'What was the date of the transaction?'
