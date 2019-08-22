d4c9cacc64b628eb06686ad6647f7de63b75ea26
Muhammad Haziq <m_haziq@outlook.com>
2019-08-22 15:49:51 +0500

    remove javascript for add recommendation button

--- src/js/transaction.js
+++ src/js/transaction.js
@@ -3,20 +3,9 @@ import {setDatePicker} from "./global";
 $(document).ready(function () {
     setDatePicker();
 
-    const recommendationFieldSelector = '#id_requester_recommendation, #id_requestee_recommendation';
-    const recommendationLabelSelector = '[for="id_requester_recommendation"], [for="id_requestee_recommendation"]';
     const confirmTransactionSelector = '.confirm-transaction-fields';
 
-    $(recommendationFieldSelector).hide();
-    $(recommendationLabelSelector).hide();
     $(confirmTransactionSelector).hide();
-
-    $('#add-recommendation').on('click', function (e) {
-        e.preventDefault();
-        $(this).hide();
-        $(recommendationFieldSelector).show();
-        $(recommendationLabelSelector).show();
-    });
 
     $('#confirm-transaction').on('click', function (e) {
         e.preventDefault();
