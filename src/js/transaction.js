6f2bea75d471db91f88255f48c42eb475e2f96d0
Muhammad Haziq <m_haziq@outlook.com>
2019-08-26 15:56:06 +0500

    added auto close pop-up for success alert

--- src/js/transaction.js
+++ src/js/transaction.js
@@ -2,5 +2,7 @@ import {setDatePicker} from "./global";
 
 $(document).ready(function () {
     setDatePicker();
-
+    $("#success-alert").fadeTo(1500, 500).slideUp(500, function() {
+        $("#success-alert").slideUp(500);
+    });
 })
