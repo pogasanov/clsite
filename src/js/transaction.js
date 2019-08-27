af37c422460fa1762d04f70479e83dd43bce57d9
Muhammad Haziq <m_haziq@outlook.com>
2019-08-27 17:39:52 +0500

    updated classes and id names according to new field name

--- src/js/transaction.js
+++ src/js/transaction.js
@@ -2,14 +2,14 @@ import {setDatePicker} from "./global";
 
 $(document).ready(function () {
     setDatePicker();
-    const transactionProofImageSelector = '#id_proof_receipt_requester_image';
+    const transactionProofImageSelector = '#id_proof_receipt_image';
     $(transactionProofImageSelector).hide();
 
     $("#success-alert").fadeTo(1500, 500).slideUp(500, function() {
         $("#success-alert").slideUp(500);
     });
 
-    $("#id_proof_receipt_requester").change(function(){
+    $("#id_proof_receipt").change(function(){
         if (this.files && this.files[0]) {
             var reader = new FileReader();
 
