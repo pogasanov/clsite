a0ad1a4cc6fa7b488d41605b86a62b470d131e02
Muhammad Haziq <m_haziq@outlook.com>
2019-08-28 11:21:32 +0500

    redo forms changes for 170

--- app/transactions/forms.py
+++ app/transactions/forms.py
@@ -8,7 +8,7 @@ class TransactionForm(ModelForm):
     class Meta:
         model = Transaction
         fields = ['is_requester_principal', 'requester_review', 'date',
-                  'amount', 'currency', 'requester_recommendation', 'proof_receipt_requester']
+                  'amount', 'currency', 'requester_recommendation', 'proof_receipt']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -28,16 +28,17 @@ class TransactionForm(ModelForm):
         self.fields['requester_review'].widget = forms.RadioSelect(choices=self.fields['requester_review'].choices)
 
         self.fields['is_requester_principal'].label = 'Did one of you pay the other?'
-        self.fields['proof_receipt_requester'].label = 'Screenshot of wire transfer (Optional)'
+        self.fields['proof_receipt'].label = 'Screenshot of wire transfer (Optional)'
         self.fields['requester_recommendation'].label = 'Write a brief recommendation'
         self.fields['requester_review'].label = 'Would you work with them again?'
         self.fields['date'].label = 'What was the date of the transaction?'
         self.fields['date'].widget.attrs['class'] += ' datepicker'
 
-    def save(self, requester, requestee, commit=True):
+    def save(self, requester, requestee, is_proof_by_requester=None, commit=True):
         transaction = super().save(commit=False)
         transaction.requester = requester
         transaction.requestee = requestee
+        transaction.is_proof_by_requester = is_proof_by_requester
 
         if transaction.currency == 'USD':
             transaction.value_in_usd = transaction.amount
