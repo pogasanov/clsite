f5625fe4a99a183a2d22cbfbe7693678e4b40b98
pavel <pogasanov@gmail.com>
2019-09-13 10:55:10 +0700

    use factory Sequence where needed

--- app/profiles/factories.py
+++ app/profiles/factories.py
@@ -155,7 +155,7 @@ class ProfileFactory(factory.django.DjangoModelFactory):
         django_get_or_create = ('handle', 'email')
 
     handle = factory.LazyAttributeSequence(lambda o, n: '-'.join(o.full_name.lower().split(' ')) + str(n))
-    email = factory.LazyAttributeSequence(lambda o, n: factory.Faker('email').generate() + str(n))
+    email = factory.Sequence(lambda n: factory.Faker('email').generate() + str(n))
     full_name = factory.Faker('name')
     password = DEFAULT_USER_PASSWORD
 
