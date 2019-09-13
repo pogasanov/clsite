a9080a0777db78f87b2c9d3c059a8416f43cf441
pavel <pogasanov@gmail.com>
2019-09-13 12:27:15 +0700

    generatetransaction command will utilize existing profiles by default

--- app/transactions/management/commands/generatetransactions.py
+++ app/transactions/management/commands/generatetransactions.py
@@ -1,16 +1,28 @@
+import random
+
 from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
 
+from profiles.models import Profile
 from transactions.factories import TransactionFactory
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('transactions_count', nargs='?', type=int, default=100)
+        parser.add_argument('--generate-profiles', action='store_true')
 
     def handle(self, *args, **options):
         try:
-            TransactionFactory.create_batch(options['transactions_count'])
+            if options['generate_profiles']:
+                TransactionFactory.create_batch(options['transactions_count'])
+            else:
+                profiles = list(Profile.objects.all())
+                if not profiles:
+                    raise CommandError('You need to generate profiles first. Use generateprofiles command.')
+                for _ in range(options['transactions_count']):
+                    requester, requestee = random.sample(profiles, 2)
+                    TransactionFactory(requester=requester, requestee=requestee)
         except IntegrityError as ie:
             raise CommandError('Unable to generate dummy profiles, truncate your database and try again.', ie)
         except Exception as ex:
