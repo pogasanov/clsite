753404d72faf2d0dde4f4557851bdcbfa6daf470
pogasanov <pogasanov@gmail.com>
2019-09-15 10:52:18 +0700

    add images from psd to profile page. uses git lfs

--- src/design.js
+++ src/design.js
@@ -1,7 +1,13 @@
+// Libraries
 import 'typeface-frank-ruhl-libre'
 import 'normalize.css'
-
+// Styles
 import './scss/design/main.scss'
-
+// Images
 import './img/design/logo.png'
 import './img/design/logo-white.png'
+// Temporary profile page specific images
+import './img/design/lfs/avatar.jpg'
+import './img/design/lfs/avatar-small.jpg'
+import './img/design/lfs/avatar-small2.jpg'
+import './img/design/lfs/avatar-small3.jpg'
