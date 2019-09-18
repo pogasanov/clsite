79cdce4dd6b907c4faa7eea3e2d34230c5303462
pavel <pogasanov@gmail.com>
2019-09-18 14:24:48 +0700

    refactor tests

--- app/transactions/tests/test_commands.py
+++ app/transactions/tests/test_commands.py
@@ -7,18 +7,22 @@ from transactions.models import Transaction
 
 
 class TransactionCommandsTestCase(TestCase):
-    def test_generatetransactions(self):
-        TRANSACTIONS_TO_CREATE = 100
+    @classmethod
+    def setUpTestData(cls):
+        cls.TRANSACTIONS_TO_CREATE = 20
 
-        ProfileFactory.create_batch(size=10)
-        call_command('generatetransactions', transactions_count=TRANSACTIONS_TO_CREATE)
+    def test_generatetransactions(self):
+        PROFILES_TO_CREATE = 10
+
+        ProfileFactory.create_batch(size=PROFILES_TO_CREATE)
+        call_command('generatetransactions', transactions_count=self.TRANSACTIONS_TO_CREATE)
         transaction_count = Transaction.objects.count()
 
-        self.assertEqual(transaction_count, TRANSACTIONS_TO_CREATE)
+        self.assertEqual(transaction_count, self.TRANSACTIONS_TO_CREATE)
 
     def test_generatetransactions_without_profiles_fail(self):
         with self.assertRaises(CommandError):
             call_command('generatetransactions')
 
     def test_generatetransactions_with_profiles_flag(self):
-        call_command('generatetransactions', generate_profiles=True)
+        call_command('generatetransactions', generate_profiles=True, transactions_count=self.TRANSACTIONS_TO_CREATE)
