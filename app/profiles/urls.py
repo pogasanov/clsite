b4e8ae7b468c0b11c13d4cfa3cbd245a070c83a6
pavel <pogasanov@gmail.com>
2019-09-24 11:20:51 +0700

    move pages urls to pages app

--- app/profiles/urls.py
+++ app/profiles/urls.py
@@ -7,8 +7,6 @@ from . import views
 
 urlpatterns = \
     [
-        path('', views.index, name='home'),
-
         path('login', auth_views.LoginView.as_view(success_url='/'), name='login'),
         path('logout', auth_views.LogoutView.as_view(), name='logout'),
         path('register', views.UserRegistrationView.as_view(), name='register'),
