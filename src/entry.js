319091386b10b96dd5eaf4de29df7b111648db49
pavel <pogasanov@gmail.com>
2019-09-24 17:58:06 +0700

    fix frontend dependency paths

--- src/entry.js
+++ src/entry.js
@@ -6,10 +6,5 @@ import './scss/main.scss'
 // Images
 import './img/logo.png'
 import './img/logo-white.png'
-// Temporary profile page specific images
-import './img/lfs/avatar.jpg'
-import './img/lfs/avatar-small.jpg'
-import './img/lfs/avatar-small2.jpg'
-import './img/lfs/avatar-small3.jpg'
 // Scripts
 import './js/profile'
