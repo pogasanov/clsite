d3f323bea87223262c1c587e76a2b090657e8bf8
pavel <pogasanov@gmail.com>
2019-09-24 11:26:55 +0700

    hook pages url into global urls

--- app/clsite/urls.py
+++ app/clsite/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', include('pages.urls')),
     path('', include('profiles.urls')),
     path('', include('transactions.urls'))
 ]
