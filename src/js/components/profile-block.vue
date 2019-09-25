b72aea7fd9538bbb09dff9a18bd2f7f20cc364af
pavel <pogasanov@gmail.com>
2019-09-25 14:43:08 +0700

    profile-block to use js data instead of slot

--- src/js/components/profile-block.vue
+++ src/js/components/profile-block.vue
@@ -4,7 +4,18 @@
             <h2 class="profile-block__title">{{ title }}</h2>
         </header>
         <div class="profile-block__main">
-            <slot></slot>
+            <template v-for="(content, header) in bodyContent">
+                <h6 class="text-header">{{ header }}</h6>
+                <template v-if="content.type === 'paragraphs'">
+                    <p v-for="c in content.content">{{ c }}</p>
+                </template>
+
+                <ul :class="content.containerClass" v-else-if="content.type === 'list'">
+                    <li :class="content.itemClass" v-for="tag in content.content">
+                        {{ tag }}
+                    </li>
+                </ul>
+            </template>
         </div>
     </section>
 </template>
@@ -12,7 +23,35 @@
 <script>
     export default {
         name: "profile-block",
-        props: ['title']
+        props: ['title'],
+        data: () => {
+            return {
+                bodyContent: {
+                    Overview: {
+                        type: 'paragraphs',
+                        content: [
+                            'Part already down military really. Least once plant recently campaign.',
+                            'Subject soldier consider road continue who. Even only put baby protect structure bank. Change shoulder number side.',
+                            'Deep million system will floor form. Page change of.'
+                        ]
+                    },
+                    Languages: {
+                        type: 'list',
+                        content: [
+                            'English'
+                        ],
+                    },
+                    'Subjective Tags': {
+                        type: 'list',
+                        containerClass: 'tag-container',
+                        itemClass: 'tag',
+                        content: [
+                            'Fast'
+                        ]
+                    },
+                }
+            }
+        }
     }
 </script>
 
