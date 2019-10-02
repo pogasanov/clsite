33d1b3d428502f0fcc816edfab10393caa9dad1b
pavel <pogasanov@gmail.com>
2019-10-02 13:54:03 +0700

    serializer update languages

--- app/profiles/serializers.py
+++ app/profiles/serializers.py
@@ -7,6 +7,12 @@ class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Language
         fields = ['id', 'name', 'proficiency_level']
+        extra_kwargs = {
+            'id': {
+                'read_only': False,
+                'required': True
+            }
+        }
 
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
@@ -15,3 +21,23 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
         fields = ['handle', 'summary', 'bio', 'languages', 'law_type_tags']
+
+    def update(self, instance, validated_data):
+        # Update the book instance
+        instance.summary = validated_data['summary']
+        instance.bio = validated_data['bio']
+        instance.save()
+
+        # Delete any pages not included in the request
+        language_ids = [item['id'] for item in validated_data['language_set']]
+        for language in instance.language_set.all():
+            if language.id not in language_ids:
+                language.delete()
+
+        # Create or update page instances that are in the request
+        for item in validated_data['language_set']:
+            language = Language(id=item['id'], name=item['name'], proficiency_level=item['proficiency_level'],
+                                profile=instance)
+            language.save()
+
+        return instance
