d752b2123d8bc9e6df2974fe9abb0d488959ebf4
Muhammad Haziq <m_haziq@outlook.com>
2019-10-15 17:19:29 +0500

    temp commit

--- app/reviews/models.py
+++ app/reviews/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    REVIEW_STRONGLY_DISAGREE = 'SD'
+    REVIEW_DISAGREE = 'D'
+    REVIEW_NEUTRAL = 'N'
+    REVIEW_AGREE = 'A'
+    REVIEW_STRONGLY_AGREE = 'SA'
+    REVIEW_CHOICES = (
+        (REVIEW_STRONGLY_DISAGREE, 'Strongly Disagree'),
+        (REVIEW_DISAGREE, 'Disagree'),
+        (REVIEW_NEUTRAL, 'Neutral'),
+        (REVIEW_AGREE, 'Agree'),
+        (REVIEW_STRONGLY_AGREE, 'Strongly Agree')
+    )
+    sender = models.ForeignKey('profiles.Profile', on_delete=models.CASCADE, related_name='sender', verbose_name='Sender')
+    receiver = models.ForeignKey('profiles.Profile', on_delete=models.CASCADE, related_name='receiver', verbose_name='Receiver')
+    sender_review = models.CharField(max_length=2, choices=REVIEW_CHOICES, default=REVIEW_NEUTRAL, verbose_name='Requester\'s Review')
+    receiver_review = models.CharField(max_length=2, choices=REVIEW_CHOICES, default=REVIEW_NEUTRAL, verbose_name='Requester\'s Review')
+
