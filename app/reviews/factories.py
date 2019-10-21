0ea3478245d5ffb0bb88a63406612ab98c6c087e
Muhammad Haziq <m_haziq@outlook.com>
2019-10-21 13:18:46 +0500

    minor update factory

--- app/reviews/factories.py
+++ app/reviews/factories.py
@@ -1,5 +1,4 @@
 import random
-from io import BytesIO
 import factory.fuzzy
 import factory.random
 
@@ -13,18 +12,15 @@ class ReviewFactory(factory.django.DjangoModelFactory):
 
     sender = factory.SubFactory(ProfileFactory)
     receiver = factory.SubFactory(ProfileFactory)
-    rating = factory.fuzzy.FuzzyChoice(Review.REVIEW_CHOICES,  getter=lambda c: c[0])
-    is_sender_principal = factory.fuzzy.FuzzyChoice(Review.IS_SENDER_PRINCIPAL_CHOICES,  getter=lambda c: c[0])
-    work_description = factory.LazyFunction(lambda: "".join(
-        factory.Faker('paragraphs', nb=3).generate()) if random.random() < 0.5 else '')
-    recommendation = factory.LazyFunction(lambda: "".join(
-        factory.Faker('paragraphs', nb=3).generate()) if random.random() < 0.5 else '')
+    rating = factory.fuzzy.FuzzyChoice(Review.REVIEW_CHOICES, getter=lambda c: c[0])
+    is_sender_principal = factory.fuzzy.FuzzyChoice(Review.IS_SENDER_PRINCIPAL_CHOICES, getter=lambda c: c[0])
+    work_description = factory.LazyFunction(lambda: "".join(factory.Faker('paragraphs', nb=3).generate()))
+    recommendation = factory.LazyFunction(lambda: "".join(factory.Faker('paragraphs', nb=3).generate()))
 
     @factory.lazy_attribute
-    def deleted_status(self):
+    def is_deleted(self):
         probability = random.random()
         # 30% chance for the review to be deleted
         if probability < 0.3:
             return True
-
         return False
