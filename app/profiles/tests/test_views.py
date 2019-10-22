95dfe2b89a74b3a265acba9187afeb4c3d90aa0a
pavel <pogasanov@gmail.com>
2019-10-22 16:34:02 +0700

    test register view at named url

--- app/profiles/tests/test_views.py
+++ app/profiles/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, override_settings
+from django.urls import reverse
 
 
 @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
@@ -9,4 +10,8 @@ class RegisterViewTest(TestCase):
 
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get(self.REGISTER_URL)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
