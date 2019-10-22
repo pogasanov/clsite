617ce8b7bd36c6278f10b28abaab953138b1f549
pavel <pogasanov@gmail.com>
2019-10-22 16:35:37 +0700

    test register view uses correct template

--- app/profiles/tests/test_views.py
+++ app/profiles/tests/test_views.py
@@ -15,3 +15,8 @@ class RegisterViewTest(TestCase):
     def test_view_url_accessible_by_name(self):
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(self.REGISTER_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
