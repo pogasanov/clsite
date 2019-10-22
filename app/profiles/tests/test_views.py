3843643de624bbf3db5c51acf2a73d6cde361bfa
pavel <pogasanov@gmail.com>
2019-10-22 19:43:39 +0700

    test new user is created

--- app/profiles/tests/test_views.py
+++ app/profiles/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase, override_settings
 from django.urls import reverse
 
 from profiles.forms import ProfileCreationForm
+from profiles.models import Profile
 
 
 @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
@@ -9,6 +10,13 @@ class RegisterViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.REGISTER_URL = '/register'
+        cls.NEW_PROFILE_EMAIL = 'test@test.com'
+        cls.data_payload = {
+            'email': cls.NEW_PROFILE_EMAIL,
+            'password1': 'c0mpl1xp4ssw0rd',
+            'password2': 'c0mpl1xp4ssw0rd',
+            'agree_tos': 'on'
+        }
 
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get(self.REGISTER_URL)
@@ -26,3 +34,15 @@ class RegisterViewTest(TestCase):
     def test_view_uses_correct_form(self):
         response = self.client.get(self.REGISTER_URL)
         self.assertIsInstance(response.context['form'], ProfileCreationForm)
+
+    def test_user_created(self):
+        profiles_count_before = Profile.objects.count()
+
+        response = self.client.post(self.REGISTER_URL, data=self.data_payload)
+        self.assertRedirects(response, '/profile')
+
+        profiles_after = Profile.objects.all()
+        self.assertEqual(profiles_count_before + 1, profiles_after.count())
+
+        last_profile = profiles_after.last()
+        self.assertEqual(last_profile.email, self.NEW_PROFILE_EMAIL)
