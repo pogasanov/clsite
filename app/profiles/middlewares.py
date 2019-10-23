7f31fa71fcaa3690f5f434d1d1e109272a29c067
pavel <pogasanov@gmail.com>
2019-10-23 10:09:51 +0700

    test redirected if profile not filled

--- app/profiles/middlewares.py
+++ app/profiles/middlewares.py
@@ -1,7 +1,12 @@
+from django.http import HttpResponseRedirect
+
+
 class ProfileFilledMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        if not request.user.is_filled():
+            return HttpResponseRedirect('/profile')
         response = self.get_response(request)
         return response
--- app/profiles/tests/test_middlewares.py
+++ app/profiles/tests/test_middlewares.py
@@ -17,8 +17,18 @@ class ProfileFilledMiddlewareTest(TestCase):
 
     def test_not_redirected_if_profile_filled(self):
         user = ProfileFactory()
-        self.client.force_login(user)
+        self.request.user = user
 
         middleware = ProfileFilledMiddleware(self.get_response)
         response = middleware(self.request)
         self.assertEqual(response, self.EXPECTED_RESULT)
+
+    def test_redirected_if_profile_not_filled(self):
+        user = ProfileFactory(empty_profile=True)
+        self.request.user = user
+
+        middleware = ProfileFilledMiddleware(self.get_response)
+        response = middleware(self.request)
+        self.assertNotEqual(response, self.EXPECTED_RESULT)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response._headers['location'][1], '/profile')
