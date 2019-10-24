0a0276818d716424ad9dfd67cb6067204b89492a
Muhammad haziq <m_haziq@outlook.com>
2019-10-24 11:42:44 +0500

    minor fix created_at randomization in 30 days span

--- app/reviews/factories.py
+++ app/reviews/factories.py
@@ -1,6 +1,8 @@
 import random
 import factory.fuzzy
 import factory.random
+from datetime import datetime, timedelta
+import pytz
 
 from profiles.factories import ProfileFactory
 from reviews.models import Review
@@ -16,6 +18,7 @@ class ReviewFactory(factory.django.DjangoModelFactory):
     is_sender_principal = factory.fuzzy.FuzzyChoice(Review.IS_SENDER_PRINCIPAL_CHOICES, getter=lambda c: c[0])
     work_description_private = factory.LazyFunction(lambda: "".join(factory.Faker('paragraphs', nb=3).generate()))
     recommendation = factory.LazyFunction(lambda: "".join(factory.Faker('paragraphs', nb=3).generate()))
+    created_at = factory.fuzzy.FuzzyDateTime(datetime.now(tz=pytz.UTC) - timedelta(days=30), datetime.now(tz=pytz.UTC))
 
     @factory.lazy_attribute
     def is_deleted(self):
