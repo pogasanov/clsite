b408d72774c6ef4e85d552178d9f91ce344c6cc9
pavel <pogasanov@gmail.com>
2019-10-24 16:57:28 +0700

    cover tests for profile page

--- app/profiles/tests/test_views.py
+++ app/profiles/tests/test_views.py
@@ -67,6 +67,35 @@ class RegisterViewTest(TestCase):
 
 
 @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class ProfileViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.VIEW_URL = '/profile'
+
+    def setUp(self):
+        self.user = ProfileFactory()
+        self.client.force_login(self.user)
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(self.VIEW_URL)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('profile-proof'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(self.VIEW_URL)
+        self.assertTemplateUsed(response, 'profile-page.html')
+
+    def test_anonymous_user_cant_access(self):
+        self.client.logout()
+
+        response = self.client.get(self.VIEW_URL)
+        self.assertRedirects(response, f"{reverse('login')}?next={self.VIEW_URL}")
+
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
 class ProfileProofViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
