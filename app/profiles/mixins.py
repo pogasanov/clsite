c19bf9cda3c02d75b43b19fcb32ae5d23a6bae53
pavel <pogasanov@gmail.com>
2019-10-24 16:52:15 +0700

    refactor mixin

--- app/profiles/mixins.py
+++ app/profiles/mixins.py
@@ -4,21 +4,24 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from profiles.models import Profile
+
 
 def signup_flow_complete(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
         if not request.user.is_anonymous and request.path != reverse('profile'):
-            if not request.user.is_filled():
+            if request.user.register_status == Profile.REGISTER_STATUS_EMPTY_PROFILE:
                 messages.add_message(request, messages.ERROR, 'You should fill out profile')
                 return HttpResponseRedirect(reverse('profile'))
 
             if request.path != reverse('profile-proof'):
-                if not request.user.is_attorney_proof_submitted():
+                if request.user.register_status == Profile.REGISTER_STATUS_NO_ATTORNEY_PROOF:
                     messages.add_message(request, messages.ERROR, 'You should submit your attorney proof')
                     return HttpResponseRedirect(reverse('profile-proof'))
 
-                if request.path != reverse('profile-email-confirmation') and not request.user.email_confirmed_at:
+                if request.path != reverse('profile-email-confirmation') \
+                        and request.user.register_status == Profile.REGISTER_STATUS_EMAIL_NOT_CONFIRMED:
                     return HttpResponseRedirect(reverse('profile-email-confirmation'))
 
         return function(request, *args, **kwargs)
