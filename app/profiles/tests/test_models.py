f135c170262ce4d31e0ec52085cae2a2aecce596
pavel <pogasanov@gmail.com>
2019-10-24 13:25:55 +0700

    refactor tests

--- app/profiles/tests/test_models.py
+++ app/profiles/tests/test_models.py
@@ -26,10 +26,11 @@ class ProfileTest(TestCase):
 
     def test_email_confirmed_at_meta(self):
         profile = Profile.objects.first()
-        self.assertEqual(profile._meta.get_field('email_confirmed_at').verbose_name, 'email confirmed at')
-        self.assertTrue(profile._meta.get_field('email_confirmed_at').null)
-        self.assertTrue(profile._meta.get_field('email_confirmed_at').blank)
-        self.assertEqual(profile._meta.get_field('email_confirmed_at').default, timezone.now)
+        field = profile._meta.get_field('email_confirmed_at')
+        self.assertEqual(field.verbose_name, 'email confirmed at')
+        self.assertTrue(field.null)
+        self.assertTrue(field.blank)
+        self.assertEqual(field.default, timezone.now)
 
     def test_email_confirmed_by_default(self):
         profile = Profile.objects.first()
@@ -37,19 +38,21 @@ class ProfileTest(TestCase):
 
     def test_passport_photo_meta(self):
         profile = Profile.objects.first()
-        self.assertEqual(profile._meta.get_field('passport_photo').verbose_name, 'Passport photo')
-        self.assertEqual(profile._meta.get_field('passport_photo').upload_to, get_image_path)
-        self.assertEqual(profile._meta.get_field('passport_photo').storage, variativeStorage())
-        self.assertTrue(profile._meta.get_field('passport_photo').null)
-        self.assertTrue(profile._meta.get_field('passport_photo').blank)
+        field = profile._meta.get_field('passport_photo')
+        self.assertEqual(field.verbose_name, 'Passport photo')
+        self.assertEqual(field.upload_to, get_image_path)
+        self.assertEqual(field.storage, variativeStorage())
+        self.assertTrue(field.null)
+        self.assertTrue(field.blank)
 
     def test_bar_license_photo_meta(self):
         profile = Profile.objects.first()
-        self.assertEqual(profile._meta.get_field('bar_license_photo').verbose_name, 'Bar license photo')
-        self.assertEqual(profile._meta.get_field('bar_license_photo').upload_to, get_image_path)
-        self.assertEqual(profile._meta.get_field('bar_license_photo').storage, variativeStorage())
-        self.assertTrue(profile._meta.get_field('bar_license_photo').null)
-        self.assertTrue(profile._meta.get_field('bar_license_photo').blank)
+        field = profile._meta.get_field('bar_license_photo')
+        self.assertEqual(field.verbose_name, 'Bar license photo')
+        self.assertEqual(field.upload_to, get_image_path)
+        self.assertEqual(field.storage, variativeStorage())
+        self.assertTrue(field.null)
+        self.assertTrue(field.blank)
 
     def test_register_status_correct(self):
         profile = ProfileFactory(empty_profile=True)
