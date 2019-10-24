e5f25720c5dcebe23c5db43d8d5a1a693c2fdbdf
pavel <pogasanov@gmail.com>
2019-10-24 15:38:55 +0700

    refactor mixin

--- app/profiles/mixins.py
+++ app/profiles/mixins.py
@@ -8,16 +8,12 @@ from django.urls import reverse
 def signup_flow_complete(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
-        if not request.user.is_anonymous:
-            if request.path == reverse('profile'):
-                return function(request, *args, **kwargs)
+        if not request.user.is_anonymous and request.path != reverse('profile'):
             if not request.user.is_filled():
                 messages.add_message(request, messages.ERROR, 'You should fill out profile')
                 return HttpResponseRedirect(reverse('profile'))
 
-            if request.path == reverse('profile-proof'):
-                return function(request, *args, **kwargs)
-            if not request.user.is_attorney_proof_submitted():
+            if request.path != reverse('profile-proof') and not request.user.is_attorney_proof_submitted():
                 messages.add_message(request, messages.ERROR, 'You should submit your attorney proof')
                 return HttpResponseRedirect(reverse('profile-proof'))
 
