195fc37ae67c6772b990a550fd7564cc65c7118c
pavel <pogasanov@gmail.com>
2019-10-27 12:29:47 +0700

    stub page for privacy terms and conditions

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('privacy-terms-and-conditions', views.privacy_terms_conditions_view, name='privacy-terms-and-conditions'),
 ]
--- app/pages/views.py
+++ app/pages/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from profiles.mixins import signup_flow_complete
@@ -6,3 +7,7 @@ from profiles.mixins import signup_flow_complete
 @signup_flow_complete
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def privacy_terms_conditions_view(request):
+    return HttpResponse()
