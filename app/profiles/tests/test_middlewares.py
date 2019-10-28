49d95f4c01d6b77d332880d4475de934f60690f5
pavel <pogasanov@gmail.com>
2019-10-28 12:29:09 +0700

    internal pages middleware not redirect if not anonymous

--- app/profiles/tests/test_middlewares.py
+++ app/profiles/tests/test_middlewares.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 
+from profiles.factories import ProfileFactory
 from profiles.middlewares import InternalPagesMiddleware
 
 
@@ -11,16 +12,24 @@ class InternalPagesMiddlewareTest(TestCase):
     def setUpTestData(cls):
         cls.factory = RequestFactory()
         cls.EXPECTED_RESULT = HttpResponse('')
-        cls.view = lambda x, y: cls.EXPECTED_RESULT
 
     def setUp(self) -> None:
         self.request = self.factory.get('/')
+        self.view = lambda *args: self.EXPECTED_RESULT
         self.mm = InternalPagesMiddleware()
 
     def test_redirected_if_anonymous_user(self):
         self.request.user = AnonymousUser()
 
         response = self.mm.process_view(self.request, self.view, [], {})
-        self.assertNotEqual(response, self.EXPECTED_RESULT)
         self.assertTrue(response.status_code, 302)
+        self.assertNotEqual(response, self.EXPECTED_RESULT)
         self.assertEqual(response._headers['location'][1], f"{reverse('login')}?next=/")
+
+    def test_not_redirected_if_logged(self):
+        user = ProfileFactory()
+        self.request.user = user
+
+        response = self.mm.process_view(self.request, self.view, [], {})
+        self.assertTrue(response.status_code, 200)
+        self.assertEqual(response, self.EXPECTED_RESULT)
