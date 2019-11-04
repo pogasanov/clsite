8e6a2865c663e90ab4b2fad51557ae6bfe1c5394
pavel <pogasanov@gmail.com>
2019-11-04 18:28:20 +0700

    educations list uses modal manipulation mixin

--- src/js/components/items/educations-list.vue
+++ src/js/components/items/educations-list.vue
@@ -5,7 +5,7 @@
                 @item-clicked="showModal"
                 label="Educations"
                 new-item-label="Add new education"
-                v-model="value"
+                v-model="modalItems"
         >
             <template v-slot:default="slotProps">
                 <ul class="list list--condensed">
@@ -19,20 +19,20 @@
         </viewable-list>
 
         <modal
-                :deletable="selectedEducation !== null"
+                :deletable="!isNew"
                 @cancel="hideModal"
 
-                @delete="modalDeleteHandle"
-                @ok="modalConfirmHandle"
-                v-if="selectedEducation !== undefined"
+                @delete="deleteModal"
+                @ok="confirmModal"
+                v-if="isModalDisplayed"
         >
-            <h3 slot="header">Edit address</h3>
+            <h3 slot="header">Edit education</h3>
             <div slot="body">
                 <label for="country">Name</label>
-                <input id="country" type="text" v-model="selectedEducation.school">
+                <input id="country" type="text" v-model="modalSelectedItem.school">
 
                 <label for="state">Degree</label>
-                <input id="state" type="text" v-model="selectedEducation.degree">
+                <input id="state" type="text" v-model="modalSelectedItem.degree">
 
                 <label for="graduation_date">Date of graduation</label>
                 <date-picker
@@ -48,64 +48,46 @@
     import viewableList from '@/components/inputs/viewable-list.vue'
     import modal from "@/components/commons/modal.vue"
     import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+    import {modalManipulation} from "@/mixins";
     import Vue from 'vue'
 
     Vue.component('date-picker', DatePicker);
 
     export default {
         name: "educations-list",
+        mixins: [modalManipulation],
         components: {
             DatePicker,
             viewableList,
             modal
         },
-        model: {
-            prop: 'value',
-            event: 'change'
-        },
         props: {
-            value: Array,
             editState: Boolean,
         },
         data() {
             return {
-                selectedEducation: undefined,
-                index: undefined,
                 graduation_date: new Date(2016, 9, 16),
             }
         },
         methods: {
+            emptyItem() {
+                return {
+                    school: '',
+                    degree: '',
+                    graduation_date: ''
+                }
+            },
             showModal(index) {
-                if (index === undefined) {
-                    this.selectedEducation = undefined
-                } else if (index === null) {
-                    this.selectedEducation = {
-                        school: '',
-                        degree: '',
-                        graduation_date: '',
-                    };
+                modalManipulation.methods.showModal.call(this, index);
+                if (!this.isNew) {
+                    this.graduation_date = this.$options.filters.stringToDate(this.modalSelectedItem.graduation_date)
+                } else {
                     this.graduation_date = new Date(2016, 9, 16)
-                } else {
-                    this.selectedEducation = Object.assign({}, this.value[index]);
-                    this.graduation_date = this.$options.filters.stringToDate(this.value[index].graduation_date)
                 }
-                this.index = index
             },
-            modalConfirmHandle() {
-                this.selectedEducation.graduation_date = this.$options.filters.dateToString(this.graduation_date);
-                if (this.index === null) {
-                    this.value.push(this.selectedEducation)
-                } else {
-                    Vue.set(this.value, this.index, this.selectedEducation)
-                }
-                this.hideModal()
-            },
-            modalDeleteHandle() {
-                this.value.splice(this.index, 1);
-                this.hideModal()
-            },
-            hideModal() {
-                this.selectedEducation = undefined
+            confirmModal() {
+                this.modalSelectedItem.graduation_date = this.$options.filters.dateToString(this.graduation_date);
+                modalManipulation.methods.confirmModal.call(this)
             },
         },
     }
