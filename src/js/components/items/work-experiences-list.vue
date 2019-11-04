5ce0868362832e4dfbe667b523e7e6f06131f179
pavel <pogasanov@gmail.com>
2019-11-04 18:46:37 +0700

    work experiences uses modal manipulation mixin

--- src/js/components/items/work-experiences-list.vue
+++ src/js/components/items/work-experiences-list.vue
@@ -5,7 +5,7 @@
                 @item-clicked="showModal"
                 label="Experience"
                 new-item-label="Add new experience"
-                v-model="value"
+                v-model="modalItems"
         >
             <template v-slot:default="slotProps">
                 <ul class="list list--condensed">
@@ -19,20 +19,20 @@
         </viewable-list>
 
         <modal
-                :deletable="selectedWorkExperience !== null"
+                :deletable="!isNew"
                 @cancel="hideModal"
 
-                @delete="modalDeleteHandle"
-                @ok="modalConfirmHandle"
-                v-if="selectedWorkExperience !== undefined"
+                @delete="deleteModal"
+                @ok="confirmModal"
+                v-if="isModalDisplayed"
         >
             <h3 slot="header">Edit address</h3>
             <div slot="body">
                 <label for="country">Company</label>
-                <input id="country" type="text" v-model="selectedWorkExperience.company_name">
+                <input id="country" type="text" v-model="modalSelectedItem.company_name">
 
                 <label for="state">Position</label>
-                <input id="state" type="text" v-model="selectedWorkExperience.position">
+                <input id="state" type="text" v-model="modalSelectedItem.position">
 
                 <label for="duration">Duration</label>
                 <date-picker
@@ -49,29 +49,24 @@
     import viewableList from '@/components/inputs/viewable-list.vue'
     import modal from "@/components/commons/modal.vue"
     import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+    import {modalManipulation} from "@/mixins";
     import Vue from 'vue'
 
     Vue.component('date-picker', DatePicker);
 
     export default {
         name: "educations-list",
+        mixins: [modalManipulation],
         components: {
             DatePicker,
             viewableList,
             modal
         },
-        model: {
-            prop: 'value',
-            event: 'change'
-        },
         props: {
-            value: Array,
             editState: Boolean,
         },
         data() {
             return {
-                selectedWorkExperience: undefined,
-                index: undefined,
                 duration: {
                     start: new Date(2016, 9, 16),
                     end: new Date(2016, 9, 17)
@@ -79,47 +74,34 @@
             }
         },
         methods: {
+            emptyItem() {
+                return {
+                    company_name: '',
+                    position: '',
+                    duration: {
+                        lower: new Date(2016, 9, 16),
+                        upper: new Date(2016, 9, 17),
+                    }
+                }
+            },
             showModal(index) {
-                if (index === undefined) {
-                    this.selectedWorkExperience = undefined
-                } else if (index === null) {
-                    this.selectedWorkExperience = {
-                        company_name: '',
-                        position: '',
-                        duration: {
-                            lower: new Date(2016, 9, 16),
-                            upper: new Date(2016, 9, 17)
-                        }
-                    };
+                modalManipulation.methods.showModal.call(this, index);
+                if (!this.isNew) {
+                    this.duration = {
+                        start: this.$options.filters.stringToDate(this.modalSelectedItem.duration.lower),
+                        end: this.$options.filters.stringToDate(this.modalSelectedItem.duration.upper)
+                    }
+                } else {
                     this.duration = {
                         start: new Date(2016, 9, 16),
-                        end: new Date(2016, 9, 17)
-                    }
-                } else {
-                    this.selectedWorkExperience = Object.assign({}, this.value[index]);
-                    this.duration = {
-                        start: this.$options.filters.stringToDate(this.value[index].duration.lower),
-                        end: this.$options.filters.stringToDate(this.value[index].duration.upper)
+                        end: new Date(2016, 9, 17),
                     }
                 }
-                this.index = index
             },
-            modalConfirmHandle() {
-                this.selectedWorkExperience.duration.lower = this.$options.filters.dateToString(this.duration.start);
-                this.selectedWorkExperience.duration.upper = this.$options.filters.dateToString(this.duration.end);
-                if (this.index === null) {
-                    this.value.push(this.selectedWorkExperience)
-                } else {
-                    Vue.set(this.value, this.index, this.selectedWorkExperience)
-                }
-                this.hideModal()
-            },
-            modalDeleteHandle() {
-                this.value.splice(this.index, 1);
-                this.hideModal()
-            },
-            hideModal() {
-                this.selectedWorkExperience = undefined
+            confirmModal() {
+                this.modalSelectedItem.duration.lower = this.$options.filters.dateToString(this.duration.start);
+                this.modalSelectedItem.duration.upper = this.$options.filters.dateToString(this.duration.end);
+                modalManipulation.methods.confirmModal.call(this)
             },
         },
     }
