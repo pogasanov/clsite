11f2bd6085992bda1492eb3a2814b3658bb23e57
pavel <pogasanov@gmail.com>
2019-11-05 13:10:39 +0700

    change UX for only 1 address

--- src/js/components/profile/profile-locations.vue
+++ src/js/components/profile/profile-locations.vue
@@ -5,47 +5,69 @@
             section_id="profile-addresses"
             title="Physical locations"
     >
-        <addresses-list
+        <viewable-select
                 :edit-state="editState"
-                v-model="addresses"
+                label="Country"
+                v-model="country"
+                :options="allCountries"
         >
-        </addresses-list>
+        </viewable-select>
+
+        <viewable-select
+                :edit-state="editState"
+                label="State"
+                v-model="state"
+                :options="getStatesByCountry"
+        >
+        </viewable-select>
+
+        <viewable-input
+                :edit-state="editState"
+                label="City"
+                v-model="city"
+        >
+        </viewable-input>
     </profile-block>
 </template>
 
 <script>
+    import languages_states_choices from '../../../../app/clsite/choices/countries+states'
     import profileBlock from '@/components/commons/profile-block.vue'
-    import addressesList from '@/components/items/addresses-list.vue'
+    import viewableSelect from '@/components/inputs/viewable-select.vue'
+    import viewableInput from '@/components/inputs/viewable-input.vue'
 
     export default {
         name: "profile-locations",
         components: {
             profileBlock,
-            addressesList,
+            viewableSelect,
+            viewableInput,
         },
         props: ['about'],
         data: () => {
             return {
                 editState: false,
-                addresses: [
-                    {
-                        name: 'dummy',
-                        proficiency_level: 'dunno'
-                    }, {
-                        name: 'language',
-                        proficiency_level: 'dunno'
-                    }
-                ],
+                country: "",
+                state: "",
+                city: "",
             }
         },
         methods: {
             updateData(data) {
-                this.addresses = data.addresses
+                if (data.address) {
+                    this.country = data.address.country
+                    this.state = data.address.state
+                    this.city = data.address.city
+                }
             },
             editHandler() {
                 if (this.editState) {
                     this.$emit('update', {
-                        addresses: this.addresses,
+                        address: {
+                            country: this.country,
+                            state: this.state,
+                            city: this.city,
+                        },
                     })
                 }
                 this.editState = !this.editState
@@ -55,12 +77,28 @@
                 this.editState = !this.editState
             },
         },
+        computed: {
+            allCountries() {
+                return languages_states_choices.map(el => el.name)
+            },
+            getStatesByCountry() {
+                const choice = languages_states_choices.find(el => {
+                    return el.name === this.country
+                })
+                return choice ? choice.states : []
+            },
+        },
         created() {
             this.updateData(this.about)
         },
         watch: {
             about(newAbout, oldAbout) {
                 this.updateData(newAbout)
+            },
+            country(newCountry, oldCountry) {
+                if (this.getStatesByCountry.indexOf(this.state) === -1) {
+                    this.state = this.getStatesByCountry[0]
+                }
             }
         }
     }
