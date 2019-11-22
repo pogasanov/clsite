6e51088a06f6c3923e8acc16d154d2454dc9e6b0
Muhammad Haziq <m.haziq.frt@gmail.com>
2019-11-22 20:18:28 +0500

    generate reviews command error fix

--- app/reviews/management/commands/generatereviews.py
+++ app/reviews/management/commands/generatereviews.py
@@ -16,18 +16,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         combinations = []
         try:
+            reviews_count = options['reviews_count']
             if options['generate_profiles']:
                 ReviewFactory.create_batch(options['reviews_count'])
             else:
                 profiles = list(Profile.objects.all())
                 if not profiles:
                     raise CommandError('You need to generate profiles first. Use `generateprofiles` command.')
-                for _ in range(options['reviews_count']):
+                retries = 0
+                while len(combinations) < reviews_count:
+                    retries += 1
                     created_by, sent_to = random.sample(profiles, 2)
                     targeted_combinition = (created_by.id, sent_to.id,)
                     if targeted_combinition not in combinations:
                         ReviewFactory(created_by=created_by, sent_to=sent_to)
                         combinations.append(targeted_combinition)
+                        retries = 0
+                    if retries == 10:
+                        break
                 if len(combinations) < options['reviews_count']:
                     self.stdout.write(
                         f'{len(combinations)} reviews generated. Unable to generate more reviews. '
