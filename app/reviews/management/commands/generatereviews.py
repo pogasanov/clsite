90972daaa80492453519def2fd30741e80547431
Muhammad Haziq <m.haziq.frt@gmail.com>
2019-11-25 12:38:00 +0500

    code improvements generatereviews command

--- app/reviews/management/commands/generatereviews.py
+++ app/reviews/management/commands/generatereviews.py
@@ -16,27 +16,31 @@ class Command(BaseCommand):
         try:
             reviews_count = options['reviews_count']
             if options['generate_profiles']:
-                ReviewFactory.create_batch(options['reviews_count'])
-            else:
-                profiles = list(Profile.objects.all())
-                if not profiles:
-                    raise CommandError('You need to generate profiles first. Use `generateprofiles` command.')
-                retries = 0
-                while len(combinations) < reviews_count:
-                    retries += 1
-                    created_by, sent_to = random.sample(profiles, 2)
-                    targeted_combinition = (created_by.id, sent_to.id,)
-                    if targeted_combinition not in combinations:
-                        ReviewFactory(created_by=created_by, sent_to=sent_to)
-                        combinations.append(targeted_combinition)
-                        retries = 0
-                    if retries == 10:
-                        break
-                if len(combinations) < options['reviews_count']:
-                    self.stdout.write(
-                        f'{len(combinations)} reviews generated. Unable to generate more reviews. '
-                        f'You need to generate more profiles first. '
-                        f'Truncate your database and use `generateprofiles` command.')
+                ReviewFactory.create_batch(reviews_count)
+                return
+
+            profiles = list(Profile.objects.all())
+
+            if not profiles:
+                raise CommandError('You need to generate profiles first. Use `generateprofiles` command.')
+
+            retries = 0
+            while len(combinations) < reviews_count and retries < 10:
+                retries += 1
+                created_by, sent_to = random.sample(profiles, 2)
+                targeted_combinition = (created_by.id, sent_to.id,)
+
+                if targeted_combinition not in combinations:
+                    ReviewFactory(created_by=created_by, sent_to=sent_to)
+                    combinations.append(targeted_combinition)
+                    retries = 0
+
+            if len(combinations) < options['reviews_count']:
+                self.stdout.write(
+                    f'{len(combinations)} reviews generated. Unable to generate more reviews. '
+                    f'You need to generate more profiles first. '
+                    f'Truncate your database and use `generateprofiles` command.')
+
         except IntegrityError as ie:
             raise CommandError("Truncate your database and run this command again", ie)
         except Exception as ex:
